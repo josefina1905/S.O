d8d208459c185735ec48a40e43e6f24323b61ef1
camiloDanielli <79849665+camiloDanielli@users.noreply.github.com>
2021-12-14 17:09:08 -0300

    Update alumnos.sh

--- alumnos.sh
+++ alumnos.sh
@@ -1,187 +1,43 @@
-NUEVOOOO
-
-
-
-
-ls /grupos | grep $grupo
-
-echo "Bienvenido, seleccione una opcion"
-
-echo "1-Registrarse"
-echo "2-Opciones"
-read opc
-case $opc in 
-	1) 
-	echo "1-Alumnos"
-	echo "2-Docentes"
-	read opc1
-	;;
-
-	2)
-	echo "1-Administrador"
-	echo "2-Tutor"
-	read opc2
-	;;
-
-	*)
-
-	echo "Opcion incorrecta, intente de nuevo"
-	read opc3
-	;;
-
-esac 
-
-if [ "$opc1" == "1" ]; then 
-	echo "Ingrese su Ci"
-	read Ci
-	echo "Ingrese su nombre"
-	read nombre
-	echo "Ingrese su apellido"
-	read apellido
-	echo "Ingrese su telefono"
-	read telefono 
-	echo "Ingrese el grupo al que corresponda el alumno"
-	read grupo	
-	echo "$Ci - $nombre - $apellido - $telefono - $grupo" 
-fi
-
-
-	if [ "$opc1" == "2" ]; then 
-	echo "Ingrese su Ci"
-	read Ci
-	echo "Ingrese su nombre"
-	read nombre
-	echo "Ingrese su apellido"
-	read apellido
-	echo "Ingrese su telefono"
-	read telefono 
-	echo "Ingrese la materia"
-	read materia
-	echo "$Ci - $nombre - $apellido - $telefono - $materia"
-
-if
-
-	if [ "$opc2" == "1" ]; then 
-	echo "1-Altas Alumnos"
-	echo "2-Bajas Alumnos"
-	echo "3-Modificar Alumnos"
-	echo "4-Buscar Alumno"
-	echo "5-Asistencia Alumno"
-	echo "6-Crear Tutor"
-	read opc4
-
-fi
-
-case $opc4 in 
-	1)
-	echo "Ingrese su Ci"
-	read Ci
-	echo "Ingrese su nombre"
-	read nombre
-	echo "Ingrese su apellido"
-	read apellido
-	echo "Ingrese su telefono"
-	read telefono 
-	echo "$Ci - $nombre - $apellido - $telefono"
-	;;
-
-	2)
-	echo "Ingrese la ci del alumno"
-	read ci
-	sed -1 "/$ci/d" 
-
-
-	3)
-    	echo "Ingrese la cedula del alumno que desea cambiar"
-  	read cedula1
-	echo "Ingrese el nuevo nombre del alumno"
-	read nombre1
-	echo "Ingrese el nuevo apellido del alumno"
-	read apellido1
-	echo "Ingrese el nuevo telefono del alumno"
-	read telefono1
-	echo "Ingrese el nuevo grupo del alumno"
-	read grupo1
-	sed -i  "s $ci- $nombre - $apellido/ $cedula1 - $nombre1 - $apellido1 - $telefono1 " 
-	echo "$Ci - $nombre - $apellido - $telefono - $grupo"
-
-	
-	4)
-	echo "Ingrese la ci del alumno que quiere buscar"
-	read ci
-	cat = ci *poner donde se guarda*
-
-
-	5)
-	echo "Lista de alumnos"
-	sort *donde se guardan*
-
-	6)
-	echo echo "Ingrese la ci del Tutor"
-	read ci
-	echo "Ingrese el nombre del Tutor"
-	read nombre
-	echo "Ingrese el apellido del Tutor"
-	read apellido
-	echo "Ingrese el telefono del Tutor"
-	read telefono
-	
-esac 
-
-
-
-
-	
-
-
-
-	
-	
-	
-	
-
-
-
-	
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-r=0
-while (r=0)
-echo "que accion desea ejecutar"
-echo "1-nuevo estudiante"
-echo "2-editar estudiante"
-echo "3-borrar estudiante"
-echo "4-salir"
-read X
-case X in
+rep=1
+while [ $rep = 1 ]
+echo "seleccione una de las siguientes opciones"
+echo "1-nuevo alumno"
+echo "2-editar alumno"
+echo "3-buscar alumno"
+echo "4-borrar alumno"
+echo "x-volver"
+read x
+case $x in
 1)
-echo "ingrese la CI"
-read CI
-;;
+echo "Ingrese la Ci del alumno"
+	read Ci
+	echo "Ingrese el nombre"
+	read nombre
+	echo "Ingrese el apellido"
+	read apellido
+	echo "Ingrese el telefono"
+	read telefono 
+	echo "Ingrese el nombre del grupo que pertenece"
+	read grupo
+	
+	echo "$Ci - $nombre - $apellido - $telefono" >>  "$grupo".txt
+	echo "$Ci - $nombre - $apellido - $telefono" >> alumnos.txt;;
 2)
-echo "ingrese la CI"
-read CI;;
-3)
-echo "ingrese la CI"
-read CI;;
-4)
-r=1;;
-done
+echo "Ingrese la Ci del alumno"
+	read Ci
+	echo "a continuacion ingrese los nuevos datos"
+	echo "nombre:"
+	read nombre
+	echo "apellido:"
+	read apellido
+	echo "telefono:"
+	read telefono 
+	echo "grupo:"
+	read grupo
+	
+	grep $Ci /grupos/* echo "$Ci - $nombre - $apellido - $telefono" >>  "$grupo".txt
+	echo "$Ci - $nombre - $apellido - $telefono" >> alumnos.txt;;
+esac
+do 
 done
 exit
